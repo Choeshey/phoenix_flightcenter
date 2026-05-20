f38c15fe8142fb1705df16a1616957fc85896458
choeshey <choeshey@gmail.com>
2026-05-20 19:00:21 +0530

    vite config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',  // ✅ changed from '/phoenix_flightcenter/'
+  base:  '/phoenix_flightcenter/',
   plugins: [
     react({
       babel: {
